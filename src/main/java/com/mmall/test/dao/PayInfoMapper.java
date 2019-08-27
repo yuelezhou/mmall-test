758082bcc74174e8eb1bb900bcf0278618971373
Zhou Le <yuelezhou@163.com>
2019-08-27 19:43:04 +0800

    perpared

---
+++ src/main/java/com/mmall/test/dao/PayInfoMapper.java
@@ -0,0 +1,17 @@
+package com.mmall.test.dao;
+
+import com.mmall.test.pojo.PayInfo;
+
+public interface PayInfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(PayInfo record);
+
+    int insertSelective(PayInfo record);
+
+    PayInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(PayInfo record);
+
+    int updateByPrimaryKey(PayInfo record);
+}
